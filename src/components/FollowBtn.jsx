8152298f6d57a650f4fa39523314d6d900c21c57
joaopdg <jpgon@live.com.pt>
2022-12-05 21:08:14 +0000

    follow btn component cleanedç

--- src/components/FollowBtn.jsx
+++ src/components/FollowBtn.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useState, useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 import { useDispatch, useSelector } from "react-redux";
 import { isUpdatedFalse, isUpdatedTrue } from "../redux/isUpdatedGlobal";
@@ -8,42 +7,11 @@ const FollowBtn = (props) => {
   const { followUserId, post } = props;
   const [isFollowing, setIsFollowing] = useState(false);
   const getToken = localStorage.getItem("authToken");
-  const { isLoggedIn, user } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   const isUpdatedGlobal = useSelector((state) => state.isUpdatedGlobal.value);
   //console.log(isUpdatedGlobal);
   const dispatch = useDispatch();
-
-  const checkIfIsFollowing = async () => {
-    try {
-      const body = { followUserId };
-      const response = await axios.get(
-        `${process.env.REACT_APP_API_URL}/in/${user._id}/follow`,
-        body,
-        {
-          headers: {
-            Authorization: `Bearer ${getToken}`,
-          },
-        }
-      );
-
-      await setIsFollowing(response);
-      console.log("this is the response", response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  /*   const checkIfFollow = ()=>{
-
-    if (post.user.followers.includes(user._id)) {
-      setIsFollowing(true);
-     
-    } else if (!post.user.followers.includes(user._id)) {
-      setIsFollowing(false);
-      
-    }
-
-  } */
 
   const handleFollow = async () => {
     try {
@@ -57,30 +25,28 @@ const FollowBtn = (props) => {
           },
         }
       );
-
-      await dispatch(isUpdatedFalse());
+      dispatch(isUpdatedFalse());
     } catch (error) {
       console.log(error);
     }
   };
   //console.log("this is it", post.user.followers);
-  console.log("this is the user", user._id);
-  console.log(post);
+  // console.log("this is the user", user._id);
+  //console.log(post);
   useEffect(() => {
-    //checkIfIsFollowing();
-    if (isLoggedIn && post.user.followers.includes(user._id)) {
+    if (post.user.followers.includes(user._id)) {
       setIsFollowing(true);
-    } else if (isLoggedIn && !post.user.followers.includes(user._id)) {
+      isUpdatedTrue();
+    } else if (!post.user.followers.includes(user._id)) {
       setIsFollowing(false);
+      isUpdatedTrue();
     }
   }, [isUpdatedGlobal]);
 
   return (
-    <div>
-      <button onClick={() => handleFollow()}>
-        {isFollowing ? "following" : "follow"}
-      </button>
-    </div>
+    <button onClick={() => handleFollow()}>
+      {isFollowing ? "following" : "follow"}
+    </button>
   );
 };
 
