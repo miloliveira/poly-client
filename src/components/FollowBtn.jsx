d68f1ef84b310c9df9486a72720f8bf98ba6c6e7
Mariana <marianalucas018@gmail.com>
2022-12-05 23:37:28 +0000

    follow btn component, changes

--- src/components/FollowBtn.jsx
+++ src/components/FollowBtn.jsx
@@ -8,9 +8,7 @@ const FollowBtn = (props) => {
   const [isFollowing, setIsFollowing] = useState(false);
   const getToken = localStorage.getItem("authToken");
   const { user } = useContext(AuthContext);
-
   const isUpdatedGlobal = useSelector((state) => state.isUpdatedGlobal.value);
-  //console.log(isUpdatedGlobal);
   const dispatch = useDispatch();
 
   const handleFollow = async () => {
@@ -30,16 +28,26 @@ const FollowBtn = (props) => {
       console.log(error);
     }
   };
-  //console.log("this is it", post.user.followers);
-  // console.log("this is the user", user._id);
-  //console.log(post);
+
+  /* const checkUser = async () => {
+    const response = await axios.get(
+      `${process.env.REACT_APP_API_URL}/in/${post.user._id}`
+    );
+
+    
+    console.log("this is the response", response.data);
+  }; */
+
   useEffect(() => {
+    //checkUser();
     if (post.user.followers.includes(user._id)) {
       setIsFollowing(true);
-      isUpdatedTrue();
-    } else if (!post.user.followers.includes(user._id)) {
+      console.log("is following", isFollowing);
+      dispatch(isUpdatedTrue());
+    } else {
       setIsFollowing(false);
-      isUpdatedTrue();
+      console.log("is following", isFollowing);
+      dispatch(isUpdatedTrue());
     }
   }, [isUpdatedGlobal]);
 
