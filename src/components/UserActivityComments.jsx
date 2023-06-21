01f1e5bcf3714325936654b1b8d3fc103061145c
Mariana <marianalucas018@gmail.com>
2023-06-21 11:53:03 +0100

    added user activity components component

---
+++ src/components/UserActivityComments.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Post from "./Post";
+import { useDispatch, useSelector } from "react-redux";
+
+const UserActivityComments = (props) => {
+  const isUpdatedGlobal = useSelector((state) => state.isUpdatedGlobal.value);
+
+  const [commentActivity, setCommentActivity] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const { userId } = props;
+
+  const getCommentActivity = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/in/${userId}/commentActivity`
+      );
+      setCommentActivity(response.data);
+    } catch (error) {
+      setErrorMessage(error.response.data.errorMessage);
+      console.log(errorMessage);
+    }
+  };
+
+  useEffect(() => {
+    getCommentActivity();
+  }, [isUpdatedGlobal]);
+
+  return (
+    <div>
+      {commentActivity &&
+        commentActivity.map((post) => {
+          return <Post key={post._id} post={post} />;
+        })}
+      {commentActivity.length === 0 && <p>No activity to show</p>}
+      {errorMessage && <p>{errorMessage}</p>}
+    </div>
+  );
+};
+
+export default UserActivityComments;
