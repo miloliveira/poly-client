a5c8fb95c1c406a2ead8c54e1d4c5ce6931d169e
Mariana <marianalucas018@gmail.com>
2023-06-21 10:24:19 +0100

    revised user activity post, working

---
+++ src/components/UserActivityPosts.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Post from "./Post";
+import { useDispatch, useSelector } from "react-redux";
+const UserActivityPosts = (props) => {
+  const isUpdatedGlobal = useSelector((state) => state.isUpdatedGlobal.value);
+
+  const dispatch = useDispatch();
+  const [postActivity, setpostActivity] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(undefined);
+  const { userId } = props;
+  const getPostActivity = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API_URL}/in/${userId}/postActivity`
+      );
+      setpostActivity(response.data);
+    } catch (error) {
+      setErrorMessage(error.response.data.errorMessage);
+      console.log(errorMessage);
+    }
+  };
+
+  useEffect(() => {
+    getPostActivity();
+  }, [isUpdatedGlobal]);
+
+  return (
+    <div>
+      {postActivity &&
+        postActivity.map((post) => {
+          return <Post key={post._id} post={post} />;
+        })}
+      {postActivity.length === 0 && <p>No activity to show</p>}
+      {errorMessage && <p>{errorMessage}</p>}
+    </div>
+  );
+};
+
+export default UserActivityPosts;
