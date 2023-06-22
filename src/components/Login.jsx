80053a2c45c3454c687241e5b192026f54aed822
Mariana <marianalucas018@gmail.com>
2023-06-22 09:13:21 +0100

    ~revised headings. working

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -34,7 +34,7 @@ const Login = () => {
 
   return (
     <LoginForm onSubmit={handleLogin}>
-      <h5>Welcome back</h5>
+      <h3>Welcome back</h3>
       <label>Username or email address</label>
       <input
         type="text"
