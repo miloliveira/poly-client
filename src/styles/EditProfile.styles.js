df4d4026139f0918af032423d5fb340f6917275c
Mariana <marianalucas018@gmail.com>
2023-09-10 17:57:27 +0100

    revised settings forms style. working

--- src/styles/EditProfile.styles.js
+++ src/styles/EditProfile.styles.js
@@ -50,7 +50,7 @@ export const EditProfileForm = styled.form`
     margin-bottom: 10px;
     background-color: #497174;
     color: white;
-    width: 155px;
+    width: 10px;
     height: 30px;
     border: none;
     border-radius: 15px;
