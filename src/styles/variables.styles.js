60aa5d3a1ac3dda756edc0ab792c84f9386e42de
Mariana <marianalucas018@gmail.com>
2023-11-16 17:41:41 +0000

    updated colors variables. working

--- src/styles/variables.styles.js
+++ src/styles/variables.styles.js
@@ -1,14 +1,14 @@
 const colors = {
-    backgroundPrimary:"#EFF5F5",
-    backgroundSecondary:"#FFFFFF",
-    softHighlight:"#D6E4E5",
-    mildHighlight:"#59898d",
-    strongHighlight:"#497174",
-    fullContrast:"red",
-    border:"#d6e4e5",
-    fontWhite:"#FFFFFF"
+  backgroundPrimary: "#EFF5F5",
+  backgroundSecondary: "#FFFFFF",
+  softHighlight: "#D6E4E5",
+  mildHighlight: "#59898d",
+  strongHighlight: "#497174",
+  deleteBtn: "#8e0000",
+  deleteBtnHover: "#c60000",
+  border: "#d6e4e5",
+  fontWhite: "#FFFFFF",
+  fontContrast: "#497174",
+};
 
-}
-
-
-export {colors}; 
+export { colors };
