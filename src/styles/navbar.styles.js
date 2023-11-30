89d133fbfec51c92457593f7899b1f875b7388f1
Mariana <marianalucas018@gmail.com>
2023-11-30 15:26:57 +0000

    created StyledNavLink to custom the style.working

--- src/styles/navbar.styles.js
+++ src/styles/navbar.styles.js
@@ -1,11 +1,16 @@
 import styled from "styled-components";
 import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
 import { colors } from "./variables.styles";
+import { NavLink } from "react-bootstrap";
 
 export const StyledNavbar = styled(Navbar)`
-  background-color: ${colors.strongHighlight};
+  background-color: ${colors.softHighlight};
 `;
 
+export const StyledNavLink = styled(NavLink)`
+  color: ${colors.fontContrast};
+`;
 export const LogOutButton = styled.button`
   color: ${colors.strongHighlight};
   font-weight: bold;
