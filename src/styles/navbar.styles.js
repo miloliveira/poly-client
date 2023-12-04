8ee7dacee8617cfda5ff0eb1dc87190a79143dce
Mariana <marianalucas018@gmail.com>
2023-12-04 17:09:26 +0000

    created styledContainer componnet. addind responsiveness to the navbar width. working

--- src/styles/navbar.styles.js
+++ src/styles/navbar.styles.js
@@ -1,11 +1,26 @@
 import styled from "styled-components";
 import Navbar from "react-bootstrap/Navbar";
-import { LinkContainer } from "react-router-bootstrap";
+import Container from "react-bootstrap/Container";
 import { colors } from "./variables.styles";
 import { NavLink } from "react-bootstrap";
 
 export const StyledNavbar = styled(Navbar)`
   background-color: ${colors.softHighlight};
+`;
+
+export const StyledContainer = styled(Container)`
+  width: 50%;
+  @media screen and (max-width: 1200px) {
+    width: 60%;
+  }
+
+  @media screen and (max-width: 700px) {
+    width: 90%;
+  }
+
+  @media screen and (max-width: 400px) {
+    width: 100%;
+  }
 `;
 
 export const StyledNavLink = styled(NavLink)`
