5a7b2471a03fe5fadecfba25295b2865601d2c13
Mariana <marianalucas018@gmail.com>
2023-12-05 16:30:59 +0000

    created NavbarBrandInnerDiv styled component. updated other navbar styled components. working

--- src/styles/navbar.styles.js
+++ src/styles/navbar.styles.js
@@ -6,6 +6,9 @@ import { NavLink } from "react-bootstrap";
 
 export const StyledNavbar = styled(Navbar)`
   background-color: ${colors.softHighlight};
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const StyledContainer = styled(Container)`
@@ -21,15 +24,34 @@ export const StyledContainer = styled(Container)`
   @media screen and (max-width: 400px) {
     width: 100%;
   }
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 0;
 `;
 
 export const StyledNavLink = styled(NavLink)`
   color: ${colors.fontContrast};
+  margin: 0;
 `;
+
 export const LogOutButton = styled.button`
   color: ${colors.strongHighlight};
   font-weight: bold;
   background-color: ${colors.backgroundPrimary};
   border: none;
   border-radius: 15px;
+`;
+
+export const NavbarBrandInnerDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 0;
+  h2 {
+    margin: 0;
+  }
+  img {
+    height: 28px;
+  }
 `;
