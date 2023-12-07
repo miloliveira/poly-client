f27ad288a2941c6cb1872a1550d1de23b7da03b9
Mariana <marianalucas018@gmail.com>
2023-12-07 15:30:21 +0000

    reviewed profile page style. working

--- src/styles/profile.styles.js
+++ src/styles/profile.styles.js
@@ -18,7 +18,7 @@ export const OccupationIcon = styled(WorkIcon)`
 
 //Styled  components
 export const ProfilePage = styled.div`
-  width: 100vw;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
