f9fa15a584d01062fdc82aae3c92586e2802c365
Mariana <marianalucas018@gmail.com>
2023-12-14 19:29:33 +0000

    updated Redux store configuration with alertOnScreenReducer. working

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import isUpdatedGlobalReducer from "./isUpdatedGlobalSlice";
+import alertOnScreenReducer from "./showAlertSlice";
 export default configureStore({
   reducer: {
     isUpdatedGlobal: isUpdatedGlobalReducer,
+    alertOnScreen: alertOnScreenReducer,
   },
 });
