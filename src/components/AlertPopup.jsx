981baba5833281a866b3f40d9ba450e37d0e3c7a
Mariana <marianalucas018@gmail.com>
2023-12-14 19:46:14 +0000

    Renamed component to AlertPopup. Configured alertOnScreen state and reducers in alertPopup component. working

---
+++ src/components/AlertPopup.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import Alert from "@mui/material/Alert";
+import IconButton from "@mui/material/IconButton";
+import Collapse from "@mui/material/Collapse";
+import CloseIcon from "@mui/icons-material/Close";
+
+import { hideAlert } from "../redux/showAlertSlice";
+import { useDispatch, useSelector } from "react-redux";
+
+import { AlertBox } from "../styles/post.styles";
+
+const AlertPopup = () => {
+  const [open, setOpen] = React.useState(true);
+
+  const dispatch = useDispatch();
+  const alertSeverity = useSelector(
+    (state) => state.alertOnScreen.alertSettings.severity
+  );
+  const alertMessage = useSelector(
+    (state) => state.alertOnScreen.alertSettings.message
+  );
+
+  return (
+    <AlertBox>
+      <Collapse in={open}>
+        <Alert
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setOpen(false);
+                dispatch(hideAlert());
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+          severity={alertSeverity}
+        >
+          {alertMessage}
+        </Alert>
+      </Collapse>
+    </AlertBox>
+  );
+};
+
+export default AlertPopup;
