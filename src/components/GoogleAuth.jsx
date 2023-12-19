2ee25f785378043cd2fd5cc41326db08d4be08f9
Mariana <marianalucas018@gmail.com>
2023-12-19 12:01:17 +0000

    Added comments to improve code readability in googleAuth component.

--- src/components/GoogleAuth.jsx
+++ src/components/GoogleAuth.jsx
@@ -1,28 +1,28 @@
+// Dependencies
 import React, { useContext, useState, useEffect } from "react";
 import axios from "axios";
-
 import { auth, provider } from "../firebase-config";
 import { signInWithRedirect, onAuthStateChanged } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { GoogleButton } from "react-google-button";
-
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 
 const GoogleAuth = () => {
+  // State and context variables
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
-
   const { storeToken, authenticateUser } = useContext(AuthContext);
 
+  // Navigation function
   const navigate = useNavigate();
-
+  // Firebase authentication state
   const [firebaseUser] = useAuthState(auth);
-
+  // Function to initiate Google sign-in
   const signInWithGoogle = async () => {
     try {
       await signInWithRedirect(auth, provider);
@@ -30,7 +30,7 @@ const GoogleAuth = () => {
       setErrorMessage("Google authentication failed");
     }
   };
-
+  // Effect to update local state based on Firebase authentication changes
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setEmail(currentUser.email);
@@ -43,24 +43,30 @@ const GoogleAuth = () => {
       unsubscribe();
     };
   }, []);
-
+  // Function to handle Google authentication
   const handleGoogleAuth = async () => {
     try {
       const body = { email, username, name, password, imageUrl };
-
+      // Send request to the server for Google authentication
       const response = await axios.post(
         `${process.env.REACT_APP_API_URL}/auth/google-auth`,
         body
       );
+      // Store the authentication token
       await storeToken(response.data.authToken);
+
+      // Authenticate the user
       await authenticateUser();
+
+      // Clear error message and navigate to home page
       setErrorMessage("");
       navigate("/");
     } catch (error) {
+      // Handle authentication error and set error message
       setErrorMessage(error.response.data.errorMessage);
     }
   };
-
+  // Automatically trigger Google authentication when user details are available
   if (firebaseUser && email && name && username && password) {
     handleGoogleAuth();
   }
