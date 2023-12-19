e5ca57c29a9ab32a3f3c38b97adab0ac7e85671c
Mariana <marianalucas018@gmail.com>
2023-12-19 16:12:34 +0000

    Added comments to improve code readability in store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,14 @@
+// Dependencies
 import { configureStore } from "@reduxjs/toolkit";
 import isUpdatedGlobalReducer from "./isUpdatedGlobalSlice";
 import alertOnScreenReducer from "./showAlertSlice";
+
+// Configure the Redux store with multiple reducers
 export default configureStore({
   reducer: {
+    // Reducer for managing global update status
     isUpdatedGlobal: isUpdatedGlobalReducer,
+    // Reducer for handling on-screen alerts
     alertOnScreen: alertOnScreenReducer,
   },
 });
