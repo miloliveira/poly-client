17271f3732194ec93257e4c3d76c7c2932bdedbc
Mariana <marianalucas018@gmail.com>
2023-12-19 12:24:10 +0000

    Added comments to improve code readability in editCommentForm component.

--- src/components/EditCommentForm.jsx
+++ src/components/EditCommentForm.jsx
@@ -1,24 +1,34 @@
+// Dependencies
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { isUpdatedFalse } from "../redux/isUpdatedGlobalSlice";
+// Style
 import {
   EditCommentContentForm,
   EditCommentFormInnerDiv,
 } from "../styles/comment.styles";
 
 const EditCommentForm = (props) => {
+  // Destructure props
   const { commentId, content, setEditing } = props;
-  const dispatch = useDispatch();
-  const isUpdatedGlobal = useSelector((state) => state.isUpdatedGlobal.value);
+  // State variables
   const [updatedContent, setUpdatedContent] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
+  // Get authentication token from local storage
   const getToken = localStorage.getItem("authToken");
 
+  // Redux dispatch
+  const dispatch = useDispatch();
+
+  // Function to handle comment editing
   const handleEditComment = async (e) => {
     try {
+      // Prevent the default form submission
       e.preventDefault();
+      // Request body with updated comment content
       const body = { content: updatedContent };
+      // Send request to update the comment
       await axios.put(
         `${process.env.REACT_APP_API_URL}/comment-update/${commentId}`,
         body,
@@ -28,14 +38,16 @@ const EditCommentForm = (props) => {
           },
         }
       );
-
+      // Clear error message and update state to indicate comment update
       setErrorMessage("");
       await setEditing(false);
       dispatch(isUpdatedFalse());
     } catch (error) {
+      // Handle errors and set error message
       setErrorMessage(error.response.data.errorMessage);
     }
   };
+  // Effect to set initial state with comment content
   useEffect(() => {
     setUpdatedContent(content);
   }, []);
