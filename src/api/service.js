3b17dc740e6f37b40e38e63dbced62d59cc59c58
Mariana <marianalucas018@gmail.com>
2023-12-19 15:57:30 +0000

    Added comments to improve code readability in service.js.

--- src/api/service.js
+++ src/api/service.js
@@ -1,14 +1,19 @@
+// Dependencies
 import axios from "axios";
 
+// Create an instance of axios with a base URL configured from environment variables
 const api = axios.create({
   baseURL: `${process.env.REACT_APP_API_URL}`,
 });
 
+// Define an error handler function to be used for handling HTTP request errors
 const errorHandler = (error) => {
   throw error;
 };
 
+// Function to upload an image using the configured axios instance
 const uploadImage = (file) => {
+  // Make a POST request to the "/upload" endpoint with the provided file data
   return api
     .post("/upload", file)
     .then((res) => res.data)
