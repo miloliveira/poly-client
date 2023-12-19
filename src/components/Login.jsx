7b0c04640fbd79cba1ff00a22d8e39a316a80f22
Mariana <marianalucas018@gmail.com>
2023-12-19 12:02:29 +0000

    Added comments to improve code readability in login component.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,32 +1,45 @@
-import axios from "axios";
+// Dependencies
 import React, { useContext, useState } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
+
+// Style
 import {
   LoginForm,
   AuthErrorMessage,
   ErrorMessageIcon,
 } from "../styles/auth.styles";
+
 const Login = () => {
+  // State and context variables
   const [loginName, setLoginName] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState(undefined);
   const { storeToken, authenticateUser } = useContext(AuthContext);
-  const navigate = useNavigate();
 
+  // Navigation function
+  const navigate = useNavigate();
+  // Function to handle the login
   const handleLogin = async (e) => {
     try {
+      // Prevent the default form submission
       e.preventDefault();
+      // Extract login credentials from the form
       const body = { loginName, password };
-
+      // Send login request to the server
       const response = await axios.post(
         `${process.env.REACT_APP_API_URL}/auth/login`,
         body
       );
+      // Store the authentication token
       await storeToken(response.data.authToken);
+      // Authenticate the user
       await authenticateUser();
+      // Redirect the user to the home page
       navigate("/");
     } catch (error) {
+      // Handle login error and set error message
       setErrorMessage(error.response.data.errorMessage);
     }
   };
