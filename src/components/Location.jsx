ec21c11c512c0a673cf308afd0344c8ee071a7e1
Mariana <marianalucas018@gmail.com>
2023-12-19 12:01:57 +0000

    Added comments to improve code readability in location component.

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,6 +1,10 @@
+// Dependencies
 import React from "react";
+// Style
 import { LocationIcon, AboutSectionComponent } from "../styles/profile.styles";
+
 const Location = (props) => {
+  // Destructure props
   const { location } = props;
   return (
     <AboutSectionComponent>
