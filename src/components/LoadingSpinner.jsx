4fb289077ee41c340d8a112f23aa457e0199f502
Mariana <marianalucas018@gmail.com>
2023-12-19 12:01:39 +0000

    Added comments to improve code readability in loadingSpinner component.

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,6 +1,10 @@
+// Dependencies
 import React from "react";
+// Assets
 import SpinnerGif from "../assets/Dual Ring-2.1s-100px.gif";
+// Style
 import { SpinnerDiv } from "../styles/spinner.styles.js";
+
 const LoadingSpinner = () => {
   return (
     <SpinnerDiv>
