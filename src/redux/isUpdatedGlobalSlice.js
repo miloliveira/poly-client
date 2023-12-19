14d791c17225933df6afe3cc910d8ad2eb5079a7
Mariana <marianalucas018@gmail.com>
2023-12-19 16:08:29 +0000

    Added comments to improve code readability in isUpdatedGlobalSlice.js

--- src/redux/isUpdatedGlobalSlice.js
+++ src/redux/isUpdatedGlobalSlice.js
@@ -1,18 +1,25 @@
+// Creating a slice of the global state for managing when to trigger an update
 import { createSlice } from "@reduxjs/toolkit";
 
+// Define the "isUpdatedGlobal" slice with an initial state
 export const isUpdatedGlobalSlice = createSlice({
   name: "isUpdatedGlobal",
   initialState: {
     value: false,
   },
+  // Define reducers to update the state based on different actions.
   reducers: {
+    // Action to set the "value" to true, indicating an update has occurred
     isUpdatedTrue: (state) => {
       state.value = true;
     },
+    // Action to set the "value" to false, indicating no update has occurred
     isUpdatedFalse: (state) => {
       state.value = false;
     },
   },
 });
+// Export action creators for the defined reducers.
 export const { isUpdatedFalse, isUpdatedTrue } = isUpdatedGlobalSlice.actions;
+// Export the reducer to be used in the Redux store.
 export default isUpdatedGlobalSlice.reducer;
